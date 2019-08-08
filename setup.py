1c7b4d917391f174d8febacd4a5e351c693d5e81
Nonameentered <matthewshu88@gmail.com>
2019-08-08 11:04:26 -0400

    change setup to remove contextlib

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     license='BSD-3-Clause',
     description='Fits item response theory models using psytan',
     #long_description=open('README.txt').read(),
-    install_requires=['pystan', 'numpy', 'patsy', 'contextlib'],
+    install_requires=['pystan', 'numpy', 'patsy'],
     url='https://?',
     author='Daniel Furr',
     author_email='danielcfurr@gmail.com',
